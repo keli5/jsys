ac19d2a4d1cd3f70b20198f044dd38b10604f95c
keli5 <46039037+keli5@users.noreply.github.com>
2022-09-10 21:42:03 -0500

    recolor prompt symbol based on return code

--- rootfs/commands/djsh.js
+++ rootfs/commands/djsh.js
@@ -1,5 +1,6 @@
 const { getPermissions } = require("../libraries/permapi")
 const { returncode } = require("../libraries/rcodeapi")
+const paths = require("path")
 
 module.exports = {
     name: "djsh",
@@ -14,13 +15,15 @@ module.exports = {
 
         let admin = getPermissions(ctx, ctx.user).includes("admin")
         let promptChar = admin ? "#" : "$"
+        let promptCharColor = ctx.color.white
+        let parsedPromptChar = promptCharColor(promptChar)
         let usernameColor = admin ? ctx.color.red : ctx.color.green
 
         ctx.env["shell"] = "djsh"
         let fuser = usernameColor.bold(ctx.user.trim());
         let fpath = ctx.color.magenta.bold(`[${ctx.path.trim()}]`);
 
-        let shprompt = `${fuser} : ${fpath} ${promptChar} `
+        let shprompt = `${fuser} : ${fpath} ${parsedPromptChar} `
 
         ctx.rl.setPrompt(shprompt)
         ctx.rl.prompt()
@@ -29,22 +32,37 @@ module.exports = {
             line = line.split(' ')
             let command = line.shift()
             let args = line;
+            let lastReturnCode = ""
         
             if (ctx.commands[command]) {
               let result = ""
               try {
                 result = ctx.commands[command].execute(ctx, args)?.stdout || ""
+                lastReturnCode = result?.code || returncode.OK
               } catch (err) {
                 console.log(ctx.color.red(err.stack))
+                lastReturnCode = result?.code || returncode.ERROR
               }
               let newline = result ? "\n" : ""
               process.stdout.write(result + newline)
             } else {
               console.log('command not found: ' + ctx.color.red(command))
+              lastReturnCode = returncode.ERROR
             }
 
             fuser = usernameColor.bold(ctx.user.trim());
-            fpath = ctx.color.magenta.bold(`[${ctx.path.trim()}]`);
+            fpath = paths.resolve(ctx.path) // FORMATTED PATH only!
+            fpath = ctx.color.magenta.bold(`[${fpath.trim()}]`);
+
+            if (lastReturnCode == returncode.OK) {
+                promptCharColor = ctx.color.green
+            } else {
+                promptCharColor = ctx.color.red
+            }
+            parsedPromptChar = promptCharColor(promptChar)
+
+            shprompt = `${fuser} : ${fpath} ${parsedPromptChar} `
+
             ctx.rl.setPrompt(shprompt)
             
             ctx.rl.prompt();
