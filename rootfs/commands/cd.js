56312cc2f0e5fc1333fe4ddc59f2230b6b8fb54c
keli5 <46039037+keli5@users.noreply.github.com>
2022-09-10 21:41:49 -0500

    add cd

---
+++ rootfs/commands/cd.js
@@ -0,0 +1,23 @@
+const { expandPath, isDir, ePath } = require("../libraries/fsapi")
+const { returncode } = require("../libraries/rcodeapi")
+
+module.exports = {
+    name: "cd",
+    desc: "Change to another directory",
+    execute: (ctx, args) => {
+        let directory = args[0] || ctx.path
+        directory = expandPath(ctx, directory, true)
+        if (isDir(directory)) {
+            ctx.path = ePath(directory)
+            
+            return {
+                code: 0
+            }
+        } else {
+            return {
+                stdout: ePath(directory) + " is not a directory",
+                code: returncode.ERROR_NOT_DIR
+            }
+        }
+    }
+}
