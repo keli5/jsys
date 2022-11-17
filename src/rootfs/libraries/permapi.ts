f48bca4c9d45f641756e079f5c9ce9938897165c
keli5 <kel5isgod@gmail.com>
2022-11-17 19:34:58 +0000

    Fix permapi error preventing djsh login

--- src/rootfs/libraries/permapi.ts
+++ src/rootfs/libraries/permapi.ts
@@ -8,11 +8,12 @@ exports.description = "permapi.js, written by keli5 for JSys as a way to work wi
  * @param {string} user Username to get permissions for
  * @returns {array} Array of user permissions
  */
-exports.getPermissions = function (ctx, user: User) {
+exports.getPermissions = function (ctx, user) {
     let perms = []
-    user = ctx.users[user.name] // ctx.users is the users file
-    let groups = user.groups
-    perms.push(...user.permissions) // add user permissions
+    user = ctx.users[user] // ctx.users is the users file
+    let groups = user["groups"]
+    perms.push(...user["permissions"]) // add user permissions
+    console.log(perms)
     groups.forEach(group => { // for each group,
         group = ctx.groups[group] 
         perms.push(...group?.permissions || "") // add group permissions ( or none if user has a bad group )
