e19f2abc9a1b2d23e5b66754ca8a6a4e4a56e679
keli5 <46039037+keli5@users.noreply.github.com>
2023-09-26 21:49:20 -0500

    fix cnf on blank line

--- rootfs/commands/djsh.js
+++ rootfs/commands/djsh.js
@@ -15,7 +15,7 @@ module.exports = {
         }
         let admin = getPermissions(ctx, ctx.user).includes("admin")
         ctx.env["SHELL"] = "djsh"
-        
+
         if (ctx.user == "root" && ctx.users[ctx.user].pwhashed == "") console.log(ctx.color.red("You should really change the root password! Root currently has no password!"))
         updatePrompt(admin, ctx, returncode.OK)
         ctx.rl.prompt()
@@ -27,64 +27,66 @@ module.exports = {
             let lastReturnCode = ""
             let execution = undefined
             let action = undefined
-        
+
             if (ctx.commands[command]) {
-              let result = ""
-              
-              try {
-                execution = ctx.commands[command].execute(ctx, args)
-                action = execution?.action || undefined
-                result = execution?.stdout || ""
-                lastReturnCode = result?.code || returncode.OK
-              } catch (err) {
-                console.log(ctx.color.red(err.stack))
-                lastReturnCode = result?.code || returncode.ERROR_INVOCATION
-              }
-              let newline = result ? "\n" : ""
-              process.stdout.write(result + newline)
+                let result = ""
+
+                try {
+                    execution = ctx.commands[command].execute(ctx, args)
+                    action = execution?.action || undefined
+                    result = execution?.stdout || ""
+                    lastReturnCode = result?.code || returncode.OK
+                } catch (err) {
+                    console.log(ctx.color.red(err.stack))
+                    lastReturnCode = result?.code || returncode.ERROR_INVOCATION
+                }
+                let newline = result ? "\n" : ""
+                process.stdout.write(result + newline)
+            } else if (command == "") {
+                lastReturnCode = returncode.OK
             } else {
-              console.log('command not found: ' + ctx.color.red(command))
-              lastReturnCode = returncode.ERROR
+                console.log('command not found: ' + ctx.color.red(command))
+                lastReturnCode = returncode.ERROR
             }
             if (exitFlag) {
-              ctx.rl.setPrompt("Login: ")
-              exitFlag = false;
+                ctx.rl.setPrompt("Login: ")
+                exitFlag = false;
             } else {
-              updatePrompt(admin, ctx, lastReturnCode)
+                updatePrompt(admin, ctx, lastReturnCode)
             }
             if (!ctx.commands[command]?.controlsReadline || action == "reprompt") ctx.rl.prompt();
-        }); 
+        });
 
         ctx.events.on("exitShell", (ctx) => {
-          ctx.env["SHELL"] = undefined;
-          exitFlag = true;
-          ctx.commands["login"].execute(ctx) // please for the love of all that is fucking holy
+            ctx.env["SHELL"] = undefined;
+            exitFlag = true;
+            ctx.commands["login"].execute(ctx) // please for the love of all that is fucking holy
         })                                   // there has to be a better way to do this
     },
 }
 
 function updatePrompt(admin, ctx, lrc) {
-  let promptChar = admin ? "#" : "$"
-  let promptCharColor = ctx.color.white
-  let parsedPromptChar = promptCharColor(promptChar)
+    let promptChar = admin ? "#" : "$"
+    let promptCharColor = ctx.color.white
+    let parsedPromptChar = promptCharColor(promptChar)
 
-  let usernameColor = admin ? ctx.color.yellow : ctx.color.cyan
-  let fuser = usernameColor.bold(ctx.user.trim());
-  
-  let fpath = paths.resolve(ctx.path) // FORMATTED PATH only!
-  fpath = fpath.replace("/home/" + ctx.user, "~")
-  fpath = fpath.replace(/[A-Z]:\\/, "/") // windowsy moment
-  fpath = fpath.replace("\\", "/")
-  fpath = ctx.color.red.bold(`[${fpath.trim()}]`);
+    let usernameColor = admin ? ctx.color.yellow : ctx.color.cyan
+    let fuser = usernameColor.bold(ctx.user.trim());
 
-  if (lrc == returncode.OK) {
-      promptCharColor = ctx.color.green
-  } else {
-      promptCharColor = ctx.color.red
-  }
-  parsedPromptChar = promptCharColor(promptChar)
+    let fpath = paths.resolve(ctx.path) // FORMATTED PATH only!
+    fpath = fpath.replace("/home/" + ctx.user, "~")
+    fpath = fpath.replace(/[A-Z]:\\/, "/") // windowsy moment
+    fpath = fpath.replace("\\", "/")
+    fpath = ctx.color.red.bold(`[${fpath.trim()}]`);
 
-  let shprompt = `${fuser} : ${fpath} ${parsedPromptChar} `
+    if (lrc == returncode.OK) {
+        promptCharColor = ctx.color.green
+    } else {
+        promptCharColor = ctx.color.red
+    }
+    parsedPromptChar = promptCharColor(promptChar)
 
-  ctx.rl.setPrompt(shprompt)
+    let shprompt = `${fuser} : ${fpath} ${parsedPromptChar} `
+
+    ctx.rl.setPrompt(shprompt)
 }
