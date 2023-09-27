ce57103b6a368ab9a2a91ce3f43ad41a4eda0cae
keli5 <46039037+keli5@users.noreply.github.com>
2023-09-26 21:45:08 -0500

    prevent manually running nologin

--- rootfs/commands/nologin.js
+++ rootfs/commands/nologin.js
@@ -1,8 +1,15 @@
 module.exports = {
-    name: 'nologin',
-    desc: '',
-    execute: (ctx) => {
+  name: 'nologin',
+  desc: '',
+  execute: (ctx) => {
+    if (ctx.env["SHELL"]) {
+      return {
+        stdout: "nologin cannot be run manually",
+        code: 1
+      }
+    } else {
       console.log("\nthis user cannot be logged into interactively")
       ctx.commands["login"].execute(ctx)
     }
+  }
 }
